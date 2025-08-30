a9bdb00f5f183e5699e68e412197074e241700f7
sajidashaikmanduru <sajidashaikmanduru@gmail.com>
2025-08-30 10:06:59 +0530

    First commit

---
+++ src/main/java/com/klu/CalcController.java
@@ -0,0 +1,19 @@
+package com.klu;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin("*")
+public class CalcController {
+
+    @GetMapping("/add/{A}/{B}")
+    public String add(@PathVariable("A") int a, @PathVariable("B") int b) {
+        return "Addition = " + (a + b);
+    }
+}
+
